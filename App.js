c8b63dca5692185770f370bdcef53d9ec644cbf9
memphis-cloud <79505122+memphis-cloud@users.noreply.github.com>
2021-02-27 13:03:01 +0300

    fixed view

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import {View, Text} from "react-native"
 class App extends React.Component{
   render(){
     return(
+      <View>
       <Text>check dev branch</Text>
       <Text>check dev branch</Text>
       <Text>check dev branch</Text>
       <Text>check dev branch</Text>
-      <View/>
+      </View>
     )
   }
 }
