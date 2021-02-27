9c458206eb78379cd65335a9028f5b367f4b2813
memphis-cloud <79505122+memphis-cloud@users.noreply.github.com>
2021-02-27 13:46:04 +0300

    componentS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import Button from "./component/Button";
+import Button from "./components/Button";
 
 class App extends React.Component {
     state = {
