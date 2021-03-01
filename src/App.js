7be5973694c2086dd8d6819fe516f9a87c40d3b3
memphis-cloud <79505122+memphis-cloud@users.noreply.github.com>
2021-03-01 12:25:19 +0300

    start project pictures

--- src/App.js
+++ src/App.js
@@ -4,31 +4,7 @@ import {Button, ButtonDone} from "./components/Button";
 import Counter from "./components/Counter";
 
 class App extends React.Component {
-    state = {
-        Name: "Start",
-        Age: 1,
-        temp: false
-    }
-
-    // increacse1 = () => {
-    //     this.setState({count: this.state.count + 3})
-    // }
-    // increacse2 = () => {
-    //     this.setState({count: this.state.count * 2})
-    // }
-    updatePeople = () => {
-        this.setState({temp: true})
-    }
-
-    
-
     render() {
-        console.log(this.state.count)
-        if (this.state.temp) {
-            return (
-                <Text>{this.state.Name}, {this.state.Age}</Text>
-            )
-        }
         return (
             <View
                 style={{
@@ -37,20 +13,6 @@ class App extends React.Component {
                     alignItems: "center"
                 }}
             >
-                {/* {this.state.count <= 20 && (<Counter count={this.state.count}/>)} */}
-                {/* <Counter count={this.state.count} />
-                {this.state.count <= 20 && (<Button increacse={this.increacse1}/>)}
-                {this.state.count >= 20 && 
-                <> 
-                    <Button increacse={this.increacse2}/>
-                </>
-                }
-                */}
-                <Text>Name</Text>
-                <TextInput value={this.state.Name} onChangeText={Name => this.setState({Name})}/>
-                <Text>Age</Text>
-                <TextInput value={this.state.Age} onChangeText={Age => this.setState({Age})}/>
-                <ButtonDone updatePeople={this.updatePeople}/>
             </View>
         )
     }
