625bee3fd716dc1cd846986e5c9cecad993588a3
Lyoha <your_email@whatever.com>
2021-03-02 18:52:27 +0300

    na pare

---
+++ src/components/Image.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { AppRegistry, View, Image, StyleSheet, Text, ScrollView, FlatList } from "react-native";
+
+const renderItem = ({ item }) => (
+    <View>
+        <Image style={{ width: 300, height: 300 }}
+            source={{ uri: item.url }}
+        />
+        <Text>{item.title}</Text>
+    </View>
+)
+
+export default renderItem;
